d2305a84a48ae85a3c5aa963e3a1ee40690d030c
benjacot8 <benjct@gmail.com>
2026-05-08 10:53:03 -0600

    added db.json

--- crud app.js
+++ crud app.js
@@ -1,15 +1,31 @@
 // JavaScript application for Week 12 Project: Create, Read, Update, Delete App
 
-console.log("Testing connection to JavaScript file...");
-console.log("Connection successful!");
+// Beginning array for shopping list:
+let shoppingList = [
+    { id: 1, item: "Apples", quantity: 5 },
+    { id: 2, item: "Bananas", quantity: 6 },
+    { id: 3, item: "Oranges", quantity: 7 }
+];
+
+
+// Constructor for adding elements to array shoppingList:
+function addItem(id, item, quantity) {
+    shoppingList.push({ id: id, item: item, quantity: quantity });
+}
+
+
 
 
 // CREATE
-
+addItem(4, "Grapes", 8);
+addItem(5, "Strawberries", 10);
+addItem(6, "Blueberries", 12);
+addItem(7, "Cheez-Itz", 2);
+console.table(shoppingList);
 
 
 // READ
-
+const selectedItem = shoppingList.find(item => item.id === 3);
 
 
 // UPDATE
